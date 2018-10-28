0121f9cd76a921be2f15853fe31586fd8afe4f23
GeekWho <geekwho@xbc.me>
2018-10-28 07:10:08 +0000

    * 实现Go指南 练习 循环和闭包
      实现Go指南 练习 斐波那契额

--- tour.go-zh.org/exercise-fibonacci-closure.go
+++ tour.go-zh.org/exercise-fibonacci-closure.go
@@ -2,7 +2,7 @@
 * @Author: GeekWho
 * @Date:   2018-10-27 17:18:22
 * @Last Modified by:   GeekWho
-* @Last Modified time: 2018-10-27 18:19:22
+* @Last Modified time: 2018-10-28 15:08:10
 */
 package main
 
@@ -10,6 +10,7 @@ import "fmt"
 
 // fibonacci is a function that returns
 // a function that returns an int.
+// 闭包
 func fibonacci() func() int {
     a, b := 0, 1
     return func() int {
@@ -18,6 +19,7 @@ func fibonacci() func() int {
     }
 }
 
+//直接递归
 func fibonacci_1(x int) int {
     if x == 1 {
         return 1
@@ -28,14 +30,40 @@ func fibonacci_1(x int) int {
     return 0
 }
 
+//尾部递归
+func fibonacci_2(x ,a ,b int) int {
+    if x == 0 {
+        return a
+    }
+    return fibonacci_2(x-1,b,a+b)
+}
+
+//迭代
+func fibonacci_3(x int) int {
+    a,b := 1, 1
+    for x >= 2 {
+        x -= 1
+        a, b = b, a+b
+    }
+    return a
+}
+
 func main() {
     f := fibonacci()
-    for i := 1; i <= 10; i++ {
+    for i := 1; i < 10; i++ {
         fmt.Println(f())
     }
 
     //计算第几个数字
-    for i := 0; i < 10; i++ {
+    for i := 1; i < 10; i++ {
         fmt.Println(fibonacci_1(i))
+    }
+
+    for i := 1; i < 10; i++ {
+        fmt.Println(fibonacci_2(i,0,1))
+    }
+
+    for i := 1; i < 10; i++ {
+        fmt.Println(fibonacci_3(i))
     }
 }
--- tour.go-zh.org/exercise-loops-and-functions.go
+++ tour.go-zh.org/exercise-loops-and-functions.go
@@ -2,5 +2,24 @@
 * @Author: GeekWho
 * @Date:   2018-10-27 17:18:51
 * @Last Modified by:   GeekWho
-* @Last Modified time: 2018-10-27 17:18:51
+* @Last Modified time: 2018-10-28 15:09:23
 */
+package main
+
+import (
+    "fmt"
+)
+
+//判断在执行的次数内且差异小于0.001
+func Sqrt(x float64) float64 {
+     z := 1.0
+     for i :=1;(z - x) < 0.001 && i < 10;i++ {
+        z -= (z*z - x) / (2*z)
+        fmt.Println(i,z)
+     }
+     return z
+}
+
+func main() {
+    fmt.Println(Sqrt(2))
+}
