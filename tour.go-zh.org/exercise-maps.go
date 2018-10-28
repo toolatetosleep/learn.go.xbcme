84d202679182ba6ba64da65cb355142a8a677f7c
GeekWho <geekwho@xbc.me>
2018-10-28 08:53:26 +0000

    * 实现Go指南 练习 映射和切片

--- tour.go-zh.org/exercise-maps.go
+++ tour.go-zh.org/exercise-maps.go
@@ -2,5 +2,38 @@
 * @Author: GeekWho
 * @Date:   2018-10-27 17:19:39
 * @Last Modified by:   GeekWho
-* @Last Modified time: 2018-10-27 17:19:39
+* @Last Modified time: 2018-10-28 16:52:06
+* @see https://github.com/golang/tour/blob/master/wc/wc.go
 */
+package main
+
+import (
+    "golang.org/x/tour/wc"
+    "strings"
+)
+
+/**
+ * "I am learning Go!"
+ * "The quick brown fox jumped over the lazy dog."
+ * "I ate a donut. Then I ate another donut."
+ * "A man a plan a canal panama."
+ */
+func WordCount(s string) map[string]int {
+    str := strings.Fields(s)
+    m := make(map[string]int)
+    for i := 0; i < len(str); i++ {
+        k := str[i]
+        //检查当前key是否存在
+        v,ok := m[k]
+        if ok {
+            m[k] = v + 1
+        } else {
+            m[k] = 1
+        }
+    }
+    return m
+}
+
+func main() {
+    wc.Test(WordCount)
+}
--- tour.go-zh.org/exercise-slices.go
+++ tour.go-zh.org/exercise-slices.go
@@ -2,5 +2,29 @@
 * @Author: GeekWho
 * @Date:   2018-10-27 17:19:22
 * @Last Modified by:   GeekWho
-* @Last Modified time: 2018-10-27 17:19:22
+* @Last Modified time: 2018-10-28 16:28:50
+* @run https://play.golang.org/
 */
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+    //申明uint8类型的一维数组
+    var x []uint8
+    //申明存放uint8类型的二维数组
+    var y [][]uint8
+    // 循环添加元素 注意数组转换
+    for i := 0; i < dx; i++ {
+        x = append(x, uint8(i))
+    }
+    // 循环添加元素到二维数组
+    for i := 0; i < dy; i++ {
+        y = append(y,x)
+    }
+    return y
+}
+
+func main() {
+    pic.Show(Pic)
+}
