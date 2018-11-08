ac1236aa0ea656b6ee744165e10576f8fe1d8e50
GeekWho <geekwho@xbc.me>
2018-11-08 23:57:12 +0800

    * 实现Go指南 练习 图像

--- tour.go-zh.org/exercise-images.go
+++ tour.go-zh.org/exercise-images.go
@@ -2,5 +2,15 @@
 * @Author: GeekWho
 * @Date:   2018-10-27 17:21:52
 * @Last Modified by:   GeekWho
-* @Last Modified time: 2018-10-27 17:21:52
+* @Last Modified time: 2018-11-08 23:56:01
 */
+package main
+
+import "golang.org/x/tour/pic"
+
+type Image struct{}
+
+func main() {
+    m := Image{}
+    pic.ShowImage(m)
+}
